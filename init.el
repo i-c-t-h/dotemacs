dba4050c908595b42c53640c45a78c5774843f96
Geoff <g@icth.net>
2026-03-27 12:59:47 -0400

    Cleanup and seperate out macOS specific changes

--- init.el
+++ init.el
@@ -20,25 +20,4 @@
   (setq auto-package-update-hide-results t)
   (auto-package-update-maybe))
 
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-
-(setq mac-command-modifier 'control)
-(setq mac-control-modifier 'meta)
-
-(setq custom-file "~/.emacs.d/custom.el")
-(if (file-exists-p custom-file)
-    (load custom-file))
-
-(use-package zenburn-theme
-  :config
-  (load-theme 'zenburn t)
-  (set-cursor-color "red"))
-
-(set-frame-font "Hack 14" nil t)
-
-(add-to-list 'initial-frame-alist '(fullscreen . maximized))
-(split-window-right)
-
 (org-babel-load-file "~/.emacs.d/g.org")
